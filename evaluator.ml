57dbb63388ac29c6dfec3829104143eba85288a3
Marcin Fatyga <fatygamarcin@gmail.com>
2012-01-09 13:00:11 +0100

    Minor fixes

--- evaluator.ml
+++ evaluator.ml
@@ -35,21 +35,6 @@ struct
     match t with 
       | TString s -> s
       | TList l -> String.concat "" ["("; (String.concat " " (List.map print_result l)); ")"]
-
-  (* let dumpp drz = print_string (print_result (untranslate drz)); print_string "\n" *)
-
-  (* let add_bindings env vars args = *)
-  (*   let add_binding var arg =  *)
-  (*     match var with *)
-  (*       | (Core.Label v) -> Hashtbl.add env v arg *)
-  (*       | _ -> raise Core.RuntimeError *)
-  (*   in *)
-  (*   (\* print_string "dodaje do srodowiska..\n"; *\) *)
-  (*   (\* dumpp (Core.Lista vars); *\) *)
-  (*   (\* dumpp (Core.Lista args); *\) *)
-  (*   ignore (List.map2 add_binding vars args); *)
-  (*   env *)
-
       
   let rec eval (expression : Core.expr) (envi : Core.environment) =
     let rec eval_builtin b tail envi = 
@@ -80,16 +65,12 @@ struct
                 | Core.Builtin b -> (eval_builtin b tail envi)
                 | Core.Lista l -> eval (Core.Lista ((eval head envi)::tail)) envi
                 | Core.Label l -> (let Core.Procedure p = (Core.find envi l) 
-                                   in (* dumpp (Core.Lista tail);  *)
-                                   let res = p (Core.Lista (List.map (fun t -> eval t envi) tail))
-                                     (* let res = p (Core.Lista tail) *)
-                                                    envi
-                                     in (* print_string "res: "; dumpp res; *) res)
+                                   in 
+                                   let res = p (Core.Lista (List.map (fun t -> eval t envi) tail)) envi
+                                     in res)
                 | Core.Procedure p -> 
                     begin
-                      (* dumpp (Core.Lista tail); *)
                       let res = (p (Core.Lista tail) envi) in
-                        (* dumpp res; *)
                         res
                     end
                 | _ -> expression
--- repl.ml
+++ repl.ml
@@ -1,8 +1,8 @@
 open Core
 open Evaluator
 open Parser
-module Corr = (Core.Make)
 
+module Corr = (Core.Make)
 module Pars = (Parser.Make (Evaluator.Make (Corr) (Library.Make (Corr))))
 
 let reader() = Pervasives.read_line()
@@ -14,16 +14,14 @@ let rec main_loop() =
   while true do
     let wyrazenie = reader() in
       Pervasives.print_string (Pars.prompt(wyrazenie));
-      (* Pervasives.print_string wyrazenie; *)
       Pervasives.print_string "\n";
       Pervasives.print_string "> ";
       flush stdout;
-
   done;
 ;;
 
 try
-  main_loop ();
+  main_loop();
 with 
   | End_of_file -> Pervasives.print_string "goodbye!\n";
 ;;
