6737398e5f3c38cb2be2d22ff748c26b184636f4
Marcin Fatyga <fatygamarcin@gmail.com>
2012-01-13 19:18:56 +0100

    Minor changes

--- examples.scm
+++ examples.scm
@@ -1,18 +1,14 @@
-;; Notacja prefiksowa
 (+ 10 12)
 (/ 18 3 2)
 (list 1 2 3)
 (- (* 5 10) (+ 2 3 4))
 (+ (+ (+ 1 1) (+ 1 1)) (+ (+ 1 1) (+ 1 1)))
 
-(define sto-piecdziesiat 150)
 (define osiemnascie (* 3 6))
+(define sto-piecdziesiat 150)
+(define one (define jeden (define ein 1)))
+(define empty-list-value (if () (quote true) (quote false)))
 
-(if 1 (define jedynka 1) 0)
-
-
-
-;; Dynamicznie typowany
 (list 2 3 (+ 1 2 3))
 (list 1 (list 1 1) (list 1 1))
 (list (quote lista) (quote symboli))
@@ -21,27 +17,20 @@
 (cons 1 ())
 (list (quote zero) 0 ())
 
-
-
-
-;; Funkcje 
-((lambda (x) (+ x 1)) sto)
+(lambda (x) (+ x 1))
+((lambda (x) (+ x 1)) 100)
 ((lambda (x) (list x (/ x 15))) 30)
 
-(define inc (lambda (x) (+ 1 x)))
-(define factorial (lambda (x) (if (number? x) (if (< x 0) (quote positive-argument,please!) (begin (define fact (lambda (y) (if (= y 1) 1 (* y (fact (- y 1)))))) (fact x))) (quote a-number,please!))))
 (define naive-length (lambda (l) (if l (quote long!) 0)))
+(define factorial (lambda (x) (if (number? x) (if (< x 0) (quote positive-argument,please!) (begin (define fact (lambda (y) (if (= y 1) 1 (* y (fact (- y 1)))))) (fact x))) (quote a-number,please!))))
 
-
-
-
-;; Nieco ciekawsze funkcje
 (define twice (lambda (f x) (f (f x))))
 (twice (lambda (x) (* x (+ 1 x))) 2)
 
 (define map (lambda (f l) (if l (cons (f (car l)) (map f (cdr l))) (list))))
+(define inc (lambda (x) (+ 1 x)))
 (map inc (list 1 2 3 4))
 
-(define filter   (lambda (p l)    (if l         (if (p (car l))             (cons (car l) (filter p (cdr l)))             (filter p (cdr l)))         (list))))
+(define filter (lambda (p l) (if l (if (p (car l)) (cons (car l) (filter p (cdr l))) (filter p (cdr l))) (list))))
 (define lista (list 1 2 (quote i'm) 3 (quote a) 1000 (quote symbol!) 100))
 (filter symbol? lista)
